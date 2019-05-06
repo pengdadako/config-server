09726c41272dc1bf0bf03c6368a01dee1bb3d043
pengdadako <1183008492@qq.com>
2019-05-06 13:24:34 +0800

    第一次提交到远程

---
+++ src/main/java/com/example/clouldconfig/ClouldConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.clouldconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClouldConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClouldConfigApplication.class, args);
+    }
+
+}
